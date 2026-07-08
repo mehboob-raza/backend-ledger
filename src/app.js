a17e51608c0510b7f845cbb02d4eaf69e1c334b7
unknown <razamehboob61@gmail.com>
2026-07-08 23:39:35 +0300

    added

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
     res.send("Ledger Service is up and running")
 })
 
-app.use("/api/auth", authRouter)
-app.use("/api/accounts", accountRouter)
-app.use("/api/transactions", transactionRoutes)
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/accounts", accountRouter)
+app.use("/api/v1/transactions", transactionRoutes)
 
 module.exports = app
